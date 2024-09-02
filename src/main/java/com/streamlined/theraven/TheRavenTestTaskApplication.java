b8254074e2b7682f25a20930e999266454097e0b
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-09-02 14:31:33 +0300

    Initial project setup

---
+++ src/main/java/com/streamlined/theraven/TheRavenTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.streamlined.theraven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRavenTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRavenTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streamlined/theraven/TheRavenTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamlined.theraven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRavenTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
